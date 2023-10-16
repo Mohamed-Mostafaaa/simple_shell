c003355a7b5863400b5edbe1cd799d1fa9ed8530
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-10-16 14:11:01 +0300

    Header file

--- shell.h
+++ shell.h
@@ -1,5 +1,20 @@
 /*str_fun.c*/
 int _strlen(char *);
 int _strcmp(char *, char *);
-int *starts_with(const char *, const char *);
-int *_strcat(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/*str_fun2.c*/
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/*str_fun3.c*/
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+
+/*str_fun4.c*/
+char **_strtow(char *, char *);
+char **_strtow2(char *, char);

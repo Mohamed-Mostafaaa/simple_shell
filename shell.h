50fa3c1c0ee59e7d71d345975796a13b632cb753
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-10-18 04:30:41 +0300

    more function

--- shell.h
+++ shell.h
@@ -12,6 +12,26 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* for convert_number */
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
@@ -22,6 +42,25 @@ char *_memset(char *, char, unsigned int);
 void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
 int bfree(void **);
+
+/* liststr model */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+/* more functions */
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+
+int print_d(int input, int);
+void print_error(info_t *, char *);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+int _erratoi(char *);
 
 /* string functions */
 int _strlen(char *);

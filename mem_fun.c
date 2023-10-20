0aa666d8bab9cadb426357a04838fd1454f84e1a
Mohamed_Mostafaaa <momostafa110022@gmail.com>
2023-10-20 06:22:05 -0700

    memroy functions

--- mem_fun.c
+++ mem_fun.c
@@ -15,87 +15,95 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (i < n)
-    {
-        s[i] = b;
-        i++;
-    }
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
 
 /**
  * ffree - frees a string of strings
+ *
  * @hh: string of strings
+ *
+ * Return: None
  */
 
 void ffree(char **hh)
 {
-    char **b = hh;
+	char **b = hh;
 
-    if (!hh)
-    {
-        return;
-    }
-    while (*hh)
-    {
-        free(*hh++);
-    }
-    free(b);
+	if (!hh)
+	{
+		return;
+	}
+	while (*hh)
+	{
+		free(*hh++);
+	}
+	free(b);
 }
 
 /**
  * _realloc - reallocates a block of memory
+ *
  * @pt: pointer to previous malloc'ated block
  * @old: byte size of previous block
  * @new: byte size of new block
+ *
+ * Return: pt
  */
 
 void *_realloc(void *pt, unsigned int old, unsigned int new)
 {
-    char *p;
-    if (!pt)
-    {
-        return (malloc(new));
-    }
-    if (!new)
-    {
-        return (free(pt), NULL)
-    }
-    if (new == old)
-    {
-        return (pt);
-    }
-    p = malloc(new);
-    if (!p)
-    {
-        return (NULL);
-    }
-    old = old < new ? old : new;
-    while (old)
-    {
-        p[old] = ((char *)pt)[old];
-    }
-    free(pt);
-    return (p);
+	char *p;
+
+	if (!pt)
+	{
+		return (malloc(new));
+	}
+	if (!new)
+	{
+		return (free(pt), NULL);
+	}
+	if (new == old)
+	{
+		return (pt);
+	}
+	p = malloc(new);
+	if (!p)
+	{
+		return (NULL);
+	}
+	old = old < new ? old : new;
+	while (old)
+	{
+		p[old] = ((char *)pt)[old];
+	}
+	free(pt);
+	return (p);
 }
 
 /**
  * bfree - frees a pointer and NULLs the address
- * @pt - address of the pointer to free
+ *
+ * @pt: address of the pointer to free
  *
  * Return: 1 if freed, otherwise 0
  */
 
 int bfree(void **pt)
 {
-    if (!pt && *pt)
-    {
-        free(*pt);
-        *pt = NULL;
-        return (1);
-    }
-    return (0);
+	if (!pt && *pt)
+	{
+		free(*pt);
+		*pt = NULL;
+		return (1);
+	}
+	return (0);
 }

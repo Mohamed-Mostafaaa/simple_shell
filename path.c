a6ef0cc30cbd11b4c4ed0465b48948848b318507
Mohamed_Mostafaaa <momostafa110022@gmail.com>
2023-10-20 06:59:10 -0700

    path

--- path.c
+++ path.c
@@ -10,41 +10,45 @@
 
 int is_cmd(info_t *inf, char *path)
 {
-    struct stat st;
+	struct stat st;
 
-    (void)inf;
-    if (!path || stat(path, &st))
-    {
-        return (0);
-    }
-    if (st.st_mode & S_IFREG)
-    {
-        return (1);
-    }
-    return (0);
+	(void)inf;
+
+	if (!path || stat(path, &st))
+	{
+		return (0);
+	}
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
 }
 
 /**
  * dup_chars - duplicates characters
+ *
  * @pathstr: the PATH string
  * @start: starting index
  * @stop: stopping index
+ *
+ * Return: buffer charcter
  */
 
 char *dup_chars(char *pathstr, int start, int stop)
 {
-    static char buf[1024];
-    int i = 0, k = 0;
+	static char buf[1024];
+	int i = 0, k = 0;
 
-    for (k = 0; i = start; i < stop; i++)
-    {
-        if (pathstr[i] != ':')
-        {
-            buf[k++] = pathstr[i];
-        }
-    }
-    buf[k] = 0;
-    return (buf);
+	for (k = 0; i = start; i < stop; i++)
+	{
+		if (pathstr[i] != ':')
+		{
+			buf[k++] = pathstr[i];
+		}
+	}
+	buf[k] = 0;
+	return (buf);
 }
 
 /**
@@ -58,41 +62,41 @@ char *dup_chars(char *pathstr, int start, int stop)
 
 char *find_path(info_t *inf, char *pathstr, char *cmd)
 {
-    int i = 0, curr_pos = 0;
-    char *path;
+	int i = 0, curr_pos = 0;
+	char *path;
 
-    if (!pathstr)
-        return (NULL);
-    if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
-    {
-        if (is_cmd(inf, cmd))
-            return (cmd);
-    }
-    while (1)
-    {
-        if (!pathstr[i] || pathstr[i] == ':')
-        {
-            path = dup_chars(pathstr, curr_pos, i);
-            if (!*path)
-            {
-                _strcat(path, cmd);
-            }
-            else
-            {
-                _strcat(path, "/");
-                _strcat(path, cmd);
-            }
-            if (is_cmd(inf, path))
-            {
-                return (path);
-            }
-            if (!pathstr[i])
-            {
-                break;
-            }
-            curr_pos = i;
-        }
-        i++;
-    }
-    return (NULL);
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(inf, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+			{
+				_strcat(path, cmd);
+			}
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(inf, path))
+			{
+				return (path);
+			}
+			if (!pathstr[i])
+			{
+				break;
+			}
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
 }

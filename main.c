7f4fa1397ccb2c49bbcfe3e02240bcfef481de4e
Mohamed_Mostafaaa <momostafa110022@gmail.com>
2023-10-20 06:08:50 -0700

    main

--- main.c
+++ main.c
@@ -10,37 +10,37 @@
 
 int main(int ec, char **ev)
 {
-    info_t inf[] = {INFO_INIT};
-    int fd = 2;
+	info_t inf[] = {INFO_INIT};
+	int fd = 2;
 
-    asm("mov %1, %0\n\t"
-        "add $3, %0"
-        : "=r"(fd)
-        : "r"(fd));
-    if (ec == 2)
-    {
-        fd = open(ev[1], O_RDONLY);
-        if (fd == -1)
-        {
-            if (errno == EACCES)
-            {
-                exit(126);
-            }
-            if (errno == ENOENT)
-            {
-                _eputs(ev[0]);
-                _eputs(": 0: Can't open ");
-                _eputs(ev[1]);
-                _eputschar('\n');
-                _eputschar(BUF_FLUSH);
-                exit(127);
-            }
-            return (EXIT_FAILURE);
-        }
-        inf->readfd = fd;
-    }
-    pop_env_list(inf);
-    read_hist(inf);
-    hsh(inf, ev);
-    return (EXIT_SUCCESS)
+	asm("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r"(fd)
+			: "r"(fd));
+	if (ec == 2)
+	{
+		fd = open(ev[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+			{
+				exit(126);
+			}
+			if (errno == ENOENT)
+			{
+				_eputs(ev[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(ev[1]);
+				_eputschar('\n');
+				_eputschar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		inf->readfd = fd;
+	}
+	pop_env_list(inf);
+	read_hist(inf);
+	hsh(inf, ev);
+	return (EXIT_SUCCESS);
 }
